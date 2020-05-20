d0cb2146e87f5d4f470699720b2a686922cecfa8
Wali Bjoerk <wgbjork@gmail.com>
2020-05-20 14:15:28 +0200

    changed various styling elements and text output on web page

--- addTask.js
+++ addTask.js
@@ -11,44 +11,42 @@ function renderTask(){
 
     for (const task of taskOutput) {
         const taskEl = document.createElement("div");
-        const {subject, participant, duetime, description} = task;
+        const {participant, duetime, description} = task;
         
-        if (document.querySelector('#dropdown').value == 'school'){
-            taskEl.innerHTML = "<div> Fag: " + subject + "<br> Deltaker(e): " + participant +
-            "<br> Frist: " + duetime + "<br>" + 
-            "Beskrivelse: " + description + "<div><br>";
-        } else {
-            taskEl.innerHTML = "<div> Deltaker(e): " + participant +
-                            "<br> Frist: " + duetime + "<br>" + 
-                            "Beskrivelse: " + description + "<div><br>";
-        }
+        
+        taskEl.innerHTML = "<div style='border: 1px solid black'> <strong>Deltaker(e):</strong> " + participant +
+                            "<br> <strong>Frist:</strong> " + duetime + "<br>" + 
+                            "<strong>Beskrivelse:</strong " + description + "<div><br>";
+        
 
-
-    }
-
-    if(document.querySelector("#dropdown").value == "school"){
         taskOutputEl.appendChild(taskEl);
-    }else if (document.querySelector("#dropdown").value == "sport"){
-        taskOutputE2.appendChild(taskE1);
-    }else if (document.querySelector("#dropdown").value == "leisure"){
-        taskOutputE3.appendChild(taskE1);
-    }else{
-        taskOutputE4.appendChild(taskE1);
+
     }
 
-}
+    
+
+    /* if(document.querySelector("#dropdown").value == "school"){
+            taskOutputEl.appendChild(taskEl);
+        }else if (document.querySelector("#dropdown").value == "sport"){
+            taskOutputE2.appendChild(taskE1);
+        }else if (document.querySelector("#dropdown").value == "leisure"){
+            taskOutputE3.appendChild(taskE1);
+        }else{
+            taskOutputE4.appendChild(taskE1);
+        } */
+
+    }
     
 // adds data to localStorage
 function addTask(event) {
     event.preventDefault();
     
     const dropdown = document.querySelector("[name = 'dropdown']").value;
-    const subject = document.querySelector("[name = 'subject']").value;
     const participant = document.querySelector("[name = 'participant']").value;
     const duetime = document.querySelector("[name = 'duetime']").value;
     const description = document.querySelector("[name = 'description']").value;
     
-    const task = {dropdown, subject, participant, duetime, description};
+    const task = {dropdown, participant, duetime, description};
     
     const taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
     taskList.push(task);
