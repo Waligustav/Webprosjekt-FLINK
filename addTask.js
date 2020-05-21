60706803fddd756a2b653274f1074424df9d0d91
venici2606 <venicia_p@live.no>
2020-05-21 17:19:16 +0200

    fixed output

--- addTask.js
+++ addTask.js
@@ -7,83 +7,40 @@ function renderTask(){
     const taskOutputEl3 = document.getElementById("combinedOutput3");
     const taskOutputEl4 = document.getElementById("combinedOutput4");
     taskOutputEl.innerHTML = "";
-    //taskOutputEl2.innerHTML = "";
-    //taskOutputEl3.innerHTML = "";
-    //taskOutputEl4.innerHTML = "";
+    taskOutputEl2.innerHTML = "";
+    taskOutputEl3.innerHTML = "";
+    taskOutputEl4.innerHTML = "";
 
     for (const task of taskOutput) {
         const taskEl = document.createElement("div");
         const {participant, duetime, description, } = task;
         
-        
+        // the output text
         taskEl.innerHTML = "<div style = 'border: 1px solid black' draggable = 'true'> <strong> Deltaker(e): </strong> " + participant +
                             "<br> <strong> Frist: </strong> " + duetime + "<br>" + 
-                            "<strong> Beskrivelse: </strong> " + description + "<div><br>";
+                            "<strong> Beskrivelse: </strong> " + description + "<div>";
         
-        /*if (document.querySelector('#dropdown').value == 'school'){
-            taskEl.innerHTML = "<div> Fag: " + subject + "<br> Deltaker(e): " + participant +
-            "<br> Frist: " + duetime + "<br>" + 
-            "Beskrivelse: " + description + "<div><br>";
-        } else {
-            taskEl.innerHTML = "<div> Deltaker(e): " + participant +
-                            "<br> Frist: " + duetime + "<br>" + 
-                            "Beskrivelse: " + description + "<div><br>";
-        } */
-
-        /*
-        if (document.querySelector("#dropdown").value == "work"){
-            taskOutputEl.appendChild(taskEl);
-
-        } else if (document.querySelector("#dropdown").value == "subjects"){
-            taskOutputEl2.appendChild(taskEl);
-
-        } else if (document.querySelector("#dropdown").value == "various"){
-            taskOutputEl3.appendChild(taskEl);
-            
-        } else{
-            taskOutputEl4.appendChild(taskEl);
-        } 
-        */
-
-       if(document.querySelector("#dropdown").value === task.dropdown) {
-        switch (document.querySelector("#dropdown").value) {
+        // into the different subjects
+        switch (document.querySelector("#category").value && task.category) {
             case "work":
                 taskOutputEl.appendChild(taskEl);
                 break;
+
             case "subjects":
                 taskOutputEl2.appendChild(taskEl);   
                 break;
+
             case "hobby":
                 taskOutputEl3.appendChild(taskEl);
                 break;
+
             case "various":
                 taskOutputEl4.appendChild(taskEl);
                 break;
         }
-        }
-
-
-
-    //taskOutputEl.appendChild(taskEl);
-    //taskOutputEl2.appendChild(taskEl);
-    //taskOutputEl3.appendChild(taskEl);
-    //taskOutputEl4.appendChild(taskEl);
-
 
     }
     
-
-    
-/*
-     if (document.querySelector("#dropdown").value == "work"){
-            taskOutputEl.appendChild(taskEl);
-        } else if (document.querySelector("#dropdown").value == "subjects"){
-            taskOutputE2.appendChild(taskE1);
-        } else if (document.querySelector("#dropdown").value == "various"){
-            taskOutputE3.appendChild(taskE1);
-        } else{
-            taskOutputE4.appendChild(taskE1);
-        } */
 
     }
     
@@ -91,12 +48,12 @@ function renderTask(){
 function addTask(event) {
     event.preventDefault();
     
-    const dropdown = document.querySelector("[name = 'dropdown']").value;
+    const category = document.querySelector("[name = 'category']").value;
     const participant = document.querySelector("[name = 'participant']").value;
     const duetime = document.querySelector("[name = 'duetime']").value;
     const description = document.querySelector("[name = 'description']").value;
     
-    const task = {dropdown, participant, duetime, description};
+    const task = {category, participant, duetime, description};
     
     const taskList = JSON.parse(window.localStorage.getItem("taskList")) || [];
     taskList.push(task);
