ddf308e815921353010328aa1af1e4bc6f576e2c
Wali Bjoerk <wgbjork@gmail.com>
2020-05-25 01:06:33 +0200

    edited darkmode to enable toggle on/off

--- archive.js
+++ archive.js
@@ -1,7 +1,22 @@
-/* Trigger darkmode function */
-let darkmodeButton = document.getElementById("darkmodeBtn");
-let bodyObj = document.getElementsByTagName("body");
+/* Trigger darkmode function 
++ Counter to increment per button-click */
+let counter = 0;
+function toggleDark(){
+    counter++;
 
-darkmodeButton.addEventListener("click", function () {
-    bodyObj[0].style.backgroundImage = "url('images/wave2.png')"
-});
+    let bodyObject = document.body;
+    bodyObject.classList.toggle("dark-mode");
+
+    /* Import objects to change */
+    let speechBubbleObject = document.getElementById("speechBubble2");
+    let archiveTextObject = document.getElementById("archiveText");
+
+    /* Even/Uneven counter toggles object change */
+    if( (counter % 2) == 1 ){
+        speechBubbleObject.style.backgroundColor = "black";
+        archiveTextObject.style.color = "white";
+    }else if( (counter % 2) == 0 ){
+        speechBubbleObject.style.backgroundColor = "white";
+        archiveTextObject.style.color = "black";
+    }
+}
