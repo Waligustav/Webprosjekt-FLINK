3ab49c1188241eb30b470ce08010f8e2faa04594
Siddii190696 <siddt96@hotmail.com>
2020-05-26 19:37:39 +0200

    Drag and done complete

--- archive.js
+++ archive.js
@@ -1,81 +1,50 @@
 // Gets data from localStorage
-function renderTask(){
-    const taskOutput = JSON.parse(window.localStorage.getItem("taskList")) || [];
 
+function renderArchive(){
+    const taskOutput = JSON.parse(window.localStorage.getItem("taskList")) || [];
     // Gets HTML-output divs
-    const taskOutputEl = document.getElementById("combinedOutput");
-    const taskOutputE2 = document.getElementById("combinedOutput2");
-    const taskOutputE3 = document.getElementById("combinedOutput3");
-    const taskOutputE4 = document.getElementById("combinedOutput4");
-    taskOutputEl.innerHTML = "";
-    taskOutputE2.innerHTML = "";
-    taskOutputE3.innerHTML = "";
-    taskOutputE4.innerHTML = "";
+    const outputWork = document.getElementById("archivedWork");
+    const outputSubject = document.getElementById("archivedSubject");
+    const outputHobby = document.getElementById("archivedHobby");
+    const outputVarious = document.getElementById("archivedVarious");
+    
+    outputWork.innerHTML = "";
+    outputSubject.innerHTML = "";
+    outputHobby.innerHTML = "";
+    outputVarious.innerHTML = "";
 
     // Loop to put local-storage values into their matching output-tab
     for (const task of taskOutput) {
-        // Page will run normally after drag-and-dropping a task
-        if (task.deleted) {
-            continue;
-        }
-
-        // Progression system
-        const taskEl = document.createElement("div");
         const {participant, duetime, description} = task;
-
-        // PROGRESSION-BAR
-        let newDiv = document.createElement("div"); 
-        let newBtn = document.createElement("button");
-        let btnText = document.createTextNode("Fullfør");
-        newBtn.appendChild(btnText);
-        newDiv.style.border = "3px solid black";
-        newDiv.style.height = "10px";
-        newDiv.style.backgroundColor = task.color; 
-         
-        newBtn.onclick = function() {
-            if(task.color === "red"){
-                task.color = "green"; 
-                localStorage.setItem('taskList', JSON.stringify(taskOutput)); 
-                newDiv.style.backgroundColor = task.color; 
-            }
-            else if(task.color === "green"){
-                task.color = "red"; 
-                localStorage.setItem('taskList', JSON.stringify(taskOutput)); 
-                newDiv.style.backgroundColor = task.color; 
-            }
-        }
-
+        const taskEl = document.createElement("div");
+        
         // Output text
-        taskEl.innerHTML = "<div id = 'taskInnerHTML' taskId = '" + task.id + "' draggable = 'true'> <strong> Deltaker(e): </strong> " + participant +
-                            "<br> <strong> Frist: </strong> " + duetime + "<br>" + "<strong> Beskrivelse: </strong> " + description  + "<br> </div>" ;
-     
+        taskEl.innerHTML = "<div id = 'taskInnerHTML' taskId = '" + task.id +"'> <strong> Deltaker(e): </strong> " + participant +
+                            "<br> <strong> Frist: </strong> " + duetime + "<br>" + "<strong> Beskrivelse: </strong> " + description  + "<br> </div><hr id='hLine'</hr>" ;
+     console.log(task.category);
         // Text into the different tabs
-        switch (document.querySelector("#category").value && task.category) {
-            case "work":
-                taskOutputEl.appendChild(taskEl); 
-                taskOutputEl.appendChild(newDiv); 
-                taskOutputEl.appendChild(newBtn); 
-                break;
+        if (task.deleted === true){
+            switch(task.category){
+                case "work": 
+                    outputWork.appendChild(taskEl);
+                    break;
 
-            case "subjects":
-                taskOutputE2.appendChild(taskEl);
-                taskOutputE2.appendChild(newDiv); 
-                taskOutputE2.appendChild(newBtn);  
-                break;
+                case "subjects":
+                    outputSubject.appendChild(taskEl); 
+                    break;
 
-            case "hobby":
-                taskOutputE3.appendChild(taskEl);
-                taskOutputE3.appendChild(newDiv); 
-                taskOutputE3.appendChild(newBtn); 
-                break;
-            case "various":
-                taskOutputE4.appendChild(taskEl);
-                taskOutputE4.appendChild(newDiv); 
-                taskOutputE4.appendChild(newBtn); 
-                break;
-        }
+                case "hobby":
+                    outputHobby.appendChild(taskEl); 
+                    break;
+
+                case "various":
+                    outputVarious.appendChild(taskEl);
+                    break;
+            }      
     }
 } 
+};
+
 
 /* Trigger darkmode function 
 + Counter to increment per button-click */
@@ -113,6 +82,3 @@ function slideDark() {
 }
 //Trigger function upon page update to reset slider
 slideDark();
-
-
-
